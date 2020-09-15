22b7d50704a770a5ed6c1ac5cd83e4930f2f5cda
Anh Le <55035519+NguyetAnhLe@users.noreply.github.com>
2020-09-15 01:06:22 -0400

    Updates

---
+++ precache-manifest.79060865f23c726c42093f57f7503cfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a61f50efb4ec38700b91a9a66e1ab2",
+    "url": "/wish-list-page/index.html"
+  },
+  {
+    "revision": "2cb0f7ef64169a5469f8",
+    "url": "/wish-list-page/static/css/main.093e069c.chunk.css"
+  },
+  {
+    "revision": "8c0bf859ddc3b0641948",
+    "url": "/wish-list-page/static/js/2.1b221a4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wish-list-page/static/js/2.1b221a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb0f7ef64169a5469f8",
+    "url": "/wish-list-page/static/js/main.c0d2a133.chunk.js"
+  },
+  {
+    "revision": "23345b5b3f965c0abcbd",
+    "url": "/wish-list-page/static/js/runtime-main.efad738b.js"
+  }
+]);
